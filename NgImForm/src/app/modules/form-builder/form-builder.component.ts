e94db56a1d733baec22ea31b3a729ac73ced75bb
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-09-22 16:16:11 +0600

    Some basic css introduced for form builder component (input, buttons, and text styles).

--- NgImForm/src/app/modules/form-builder/form-builder.component.ts
+++ NgImForm/src/app/modules/form-builder/form-builder.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-form-builder',
   templateUrl: './form-builder.component.html',
   styleUrls: ['./form-builder.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class FormBuilderComponent implements OnInit {
   constructor() {}
