3f6c49fdeed57e5f5076d4d06e44ce615e190674
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-10-12 16:18:50 +0600

    Form Preview textbox view basic done (should have to add validations)

--- NgImForm/src/app/modules/form-preview/form-preview.component.ts
+++ NgImForm/src/app/modules/form-preview/form-preview.component.ts
@@ -1,8 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {
+  IElementModel,
   IHorizonatalFormSectionModel,
   IHorizontalFormModel,
+  ITextBoxModel,
 } from '../../models/horizontal-form.model';
+import {
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { TEXTBOX_UTILITIES } from '../../constants/textbox-utilities.constant';
 
 @Component({
   selector: 'app-form-preview',
@@ -10,18 +19,79 @@ import {
   styleUrls: ['./form-preview.component.css'],
 })
 export class FormPreviewComponent implements OnInit {
-  @Input() form: IHorizontalFormModel | null | undefined;
+  @Input() preset: IHorizontalFormModel | null | undefined;
 
   sections: IHorizonatalFormSectionModel[] = [];
+
+  form: FormGroup = new FormGroup({});
+  isSubmitClicked: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
-    if (!this.form) {
+    if (!this.preset) {
       console.error('Must need a form object to initialze the form!');
       return;
     }
 
-    this.sections = this.form.sections;
+    this.sections = this.preset.sections;
+    this.buildForm();
+  }
+
+  private buildForm(): void {
+    for (const section of this.sections) {
+      for (const element of section.elements) {
+        this.appendControl(element);
+      }
+    }
+  }
+
+  private appendControl(element: IElementModel): void {
+    if (element.textBoxComponent) {
+      this.appendTextBox(element.textBoxComponent);
+    }
+  }
+
+  private appendTextBox(textbox: ITextBoxModel): void {
+    let validators: ValidatorFn[] = [];
+
+    if (!textbox.isReadOnly && !textbox.isHidden) {
+      if (textbox.isRequired) {
+        validators.push(Validators.required);
+      }
+
+      if (textbox.validations) {
+        for (const validation of textbox.validations) {
+          const { type, min, max, minChar, maxChar } = validation;
+
+          if (type === TEXTBOX_UTILITIES.TextboxValidationsEnum.EMAIL) {
+            validators.push(Validators.email);
+          } else if (type === TEXTBOX_UTILITIES.TextboxValidationsEnum.MIN) {
+            validators.push(Validators.min(min ?? 0));
+          } else if (type === TEXTBOX_UTILITIES.TextboxValidationsEnum.MAX) {
+            validators.push(Validators.max(max ?? 0));
+          } else if (
+            type === TEXTBOX_UTILITIES.TextboxValidationsEnum.MIN_LEN
+          ) {
+            validators.push(Validators.minLength(minChar ?? 0));
+          } else if (
+            type === TEXTBOX_UTILITIES.TextboxValidationsEnum.MAX_LEN
+          ) {
+            validators.push(Validators.maxLength(maxChar ?? 0));
+          }
+        }
+      }
+
+      if (textbox.regexValidation) {
+        const { expression } = textbox.regexValidation;
+
+        validators.push(Validators.pattern(expression));
+      }
+
+      this.form.addControl(
+        textbox.name,
+        new FormControl(textbox.value, validators)
+      );
+    }
   }
 }
--- NgImForm/src/app/modules/form-preview/form-preview.module.ts
+++ NgImForm/src/app/modules/form-preview/form-preview.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormPreviewComponent } from './form-preview.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [FormPreviewComponent],
   exports: [FormPreviewComponent],
 })
