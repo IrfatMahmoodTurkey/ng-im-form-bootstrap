a837bce75023e35cd8ac8d23eff4f528fffc3d47
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-10-13 15:23:10 +0600

    File upload basic functionalities done in Form Preview
    Still have to work more on validation. Like, when field empty after browsed once, the validations are not working properly.

--- NgImForm/src/app/modules/form-preview/form-preview.component.ts
+++ NgImForm/src/app/modules/form-preview/form-preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {
   IElementModel,
+  IFileFieldModel,
   IHorizonatalFormSectionModel,
   IHorizontalFormModel,
   ITextAreaModel,
@@ -28,6 +29,22 @@ export class FormPreviewComponent implements OnInit {
   isSubmitClicked: boolean = false;
 
   textboxValidationEnum = TEXTBOX_UTILITIES.TextboxValidationsEnum;
+
+  browsedFiles: Map<
+    string,
+    {
+      required: boolean;
+      requiredMessage: string | null;
+      files: any[];
+      hasFiles: boolean;
+      sizeValid: boolean;
+      sizeValidationMessage: string | null;
+      extensionsValid: boolean;
+      extendsionValidationMessages: string[];
+      everythingIsValid: boolean;
+    }
+  > = new Map();
+
   constructor() {}
 
   ngOnInit() {
@@ -38,6 +55,7 @@ export class FormPreviewComponent implements OnInit {
 
     this.sections = this.preset.sections;
     this.buildForm();
+    this.initializeFileFieldUtilities();
   }
 
   submit(): void {
@@ -140,5 +158,200 @@ export class FormPreviewComponent implements OnInit {
       textarea.name,
       new FormControl(textarea.value, validators)
     );
+  }
+
+  private initializeFileFieldUtilities(): void {
+    if (!this.preset) return;
+
+    for (const section of this.preset.sections) {
+      for (const element of section.elements) {
+        if (element.fileFieldComponent) {
+          this.browsedFiles.set(element.fileFieldComponent.id, {
+            files: [],
+            hasFiles: false,
+            required:
+              !element.fileFieldComponent.isHidden &&
+              !element.fileFieldComponent.isReadOnly
+                ? element.fileFieldComponent.isRequired
+                : false,
+            requiredMessage: element.fileFieldComponent.requiredMessage,
+            sizeValid: true,
+            sizeValidationMessage: element.fileFieldComponent.sizeValidation
+              ? element.fileFieldComponent.sizeValidation.message
+              : null,
+            extensionsValid: true,
+            extendsionValidationMessages: [],
+            everythingIsValid:
+              !element.fileFieldComponent.isHidden &&
+              !element.fileFieldComponent.isReadOnly
+                ? element.fileFieldComponent.isRequired
+                : false,
+          });
+        }
+      }
+    }
+  }
+
+  fileChangeEvent(event: any, props: IFileFieldModel): void {
+    let current:
+      | {
+          required: boolean;
+          requiredMessage: string | null;
+          files: any[];
+          hasFiles: boolean;
+          sizeValid: boolean;
+          sizeValidationMessage: string | null;
+          extensionsValid: boolean;
+          extendsionValidationMessages: string[];
+          everythingIsValid: boolean;
+        }
+      | undefined = this.browsedFiles.get(props.id);
+
+    let toUpdate:
+      | {
+          required: boolean;
+          requiredMessage: string | null;
+          files: any[];
+          hasFiles: boolean;
+          sizeValid: boolean;
+          sizeValidationMessage: string | null;
+          extensionsValid: boolean;
+          extendsionValidationMessages: string[];
+          everythingIsValid: boolean;
+        }
+      | undefined = undefined;
+
+    if (!current) {
+      toUpdate = this.browsedFiles.get(props.id);
+    } else {
+      toUpdate = current;
+    }
+
+    if (!toUpdate) return;
+
+    if (props.isMultiple) {
+      const files = event.target.files;
+
+      toUpdate.files = files.length > 0 ? [...files] : [];
+      toUpdate.hasFiles = toUpdate.files.length > 0;
+    } else {
+      const file = event.target.files.length > 0 ? event.target.files[0] : null;
+
+      if (file) {
+        toUpdate.files = [];
+        toUpdate.files.push(file);
+      }
+
+      toUpdate.hasFiles = toUpdate.files.length > 0;
+    }
+
+    if (props.isHidden || props.isReadOnly) {
+      toUpdate.required = false;
+    } else {
+      toUpdate.required = props.isRequired;
+    }
+
+    if (props.sizeValidation) {
+      if (props.isMultiple) {
+        const files = event.target.files;
+
+        for (const file of files) {
+          const isValid: boolean = this.checkFileSize(
+            file,
+            props.sizeValidation.size
+          );
+
+          if (!isValid) {
+            toUpdate.sizeValid = false;
+            break;
+          }
+
+          toUpdate.sizeValid = true;
+        }
+      } else {
+        const file = event.target.files[0];
+
+        if (file) {
+          const isValid: boolean = this.checkFileSize(
+            file,
+            props.sizeValidation.size
+          );
+
+          toUpdate.sizeValid = isValid;
+        }
+      }
+    }
+
+    if (props.extensionValidations && props.extensionValidations.length > 0) {
+      if (props.isMultiple) {
+        const files = event.target.files;
+
+        for (const file of files) {
+          for (const exntensionValidation of props.extensionValidations) {
+            const isValid: boolean = this.checkFileExtension(
+              file,
+              exntensionValidation.extension
+            );
+
+            if (!isValid) {
+              toUpdate.extensionsValid = false;
+              toUpdate.extendsionValidationMessages.push(
+                exntensionValidation.message
+              );
+              break;
+            }
+
+            toUpdate.extensionsValid = true;
+          }
+        }
+      } else {
+        const file = event.target.files[0];
+
+        if (file) {
+          for (const exntensionValidation of props.extensionValidations) {
+            const isValid: boolean = this.checkFileExtension(
+              file,
+              exntensionValidation.extension
+            );
+
+            if (!isValid) {
+              toUpdate.extensionsValid = false;
+              toUpdate.extendsionValidationMessages.push(
+                exntensionValidation.message
+              );
+              break;
+            }
+
+            toUpdate.extensionsValid = true;
+          }
+        }
+      }
+    }
+
+    let everythingIsFine: boolean = false;
+
+    if (props.isHidden || props.isReadOnly) {
+      everythingIsFine = true;
+    } else {
+      everythingIsFine =
+        (props.isRequired ? toUpdate.hasFiles : true) &&
+        toUpdate.sizeValid &&
+        toUpdate.extensionsValid;
+    }
+
+    toUpdate.everythingIsValid = everythingIsFine;
+
+    this.browsedFiles.set(props.id, toUpdate);
+  }
+
+  private checkFileSize(file: any, sizeToCompare: number): boolean {
+    const sizeInMB: number = file.size / (1024 * 1024);
+    return sizeInMB <= sizeToCompare;
+  }
+
+  private checkFileExtension(file: any, extensionToCompare: string): boolean {
+    const extension: string = file.name.split('.').pop()?.toLowerCase();
+
+    return extension === extensionToCompare;
   }
 }
