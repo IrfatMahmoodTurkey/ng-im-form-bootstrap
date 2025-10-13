c879d7222b00da0af79e5e8c3d1d3dcf6294a8b3
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-10-13 12:00:08 +0600

    Navigate between form builder and form preview pages.

--- NgImForm/src/app/pages/form-builder-page/form-builder-page.module.ts
+++ NgImForm/src/app/pages/form-builder-page/form-builder-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilderPageComponent } from './form-builder-page.component';
 import { FormBuilderModule } from '../../modules/form-builder/form-builder.module';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, FormBuilderModule],
+  imports: [CommonModule, FormBuilderModule, RouterLink],
   declarations: [FormBuilderPageComponent],
 })
 export class FormBuilderPageModule {}
--- NgImForm/src/app/pages/form-preview-page/form-preview-page.module.ts
+++ NgImForm/src/app/pages/form-preview-page/form-preview-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormPreviewPageComponent } from './form-preview-page.component';
 import { FormPreviewModule } from '../../modules/form-preview/form-preview.module';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, FormPreviewModule],
+  imports: [CommonModule, FormPreviewModule, RouterLink],
   declarations: [FormPreviewPageComponent],
 })
 export class FormPreviewPageModule {}
