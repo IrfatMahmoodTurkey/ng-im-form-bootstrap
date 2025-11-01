cb1ab6612017e12e7f859d576623f6e3d534e692
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-11-01 12:02:37 +0600

    Shared directory removed and ButtonSpinnerComponent moved to UIModule inside Modules -> shared-modules directory in library project.

--- NgImFormBootstrap/projects/ng-im-form-bootstrap/src/lib/modules/shared-modules/ui-module/ui.module.ts
+++ NgImFormBootstrap/projects/ng-im-form-bootstrap/src/lib/modules/shared-modules/ui-module/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SpinnerDisabledButtonComponent } from './spinner-disabled-button/spinner-disabled-button.component';
 import { CommonModule } from '@angular/common';
+import { ButtonSpinnerComponent } from './button-spinner/button-spinner.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SpinnerDisabledButtonComponent],
-  exports: [SpinnerDisabledButtonComponent],
+  declarations: [SpinnerDisabledButtonComponent, ButtonSpinnerComponent],
+  exports: [SpinnerDisabledButtonComponent, ButtonSpinnerComponent],
 })
 export class UIModule {}
